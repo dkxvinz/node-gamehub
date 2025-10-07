8b4b4f3f1da5e16e8f78d87beae2eafb127455ec
kalab <wef.junthakorn774@gmail.com>
2025-10-07 19:17:03 +0700

    buid dist folder

---
+++ dist/app.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = void 0;
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const index_1 = require("./controller/index");
+const users_1 = require("./controller/users");
+const body_parser_1 = __importDefault(require("body-parser"));
+exports.app = (0, express_1.default)();
+exports.app.use((0, cors_1.default)({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+exports.app.use(body_parser_1.default.text());
+exports.app.use(body_parser_1.default.json());
+exports.app.use("/", index_1.router);
+exports.app.use("/users", users_1.router);
---
+++ dist/server.js
@@ -0,0 +1,32 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const app_1 = require("./app");
+const dbconnect_1 = __importDefault(require("./db/dbconnect"));
+const PORT = process.env.port || 3000;
+const server = http_1.default.createServer(app_1.app);
+const testDatabaseConnection = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield dbconnect_1.default.query("SELECT 1");
+        console.log("Database connected successfully!");
+    }
+    catch (err) {
+        console.error("Database connection failed:", err);
+        process.exit(1);
+    }
+});
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
